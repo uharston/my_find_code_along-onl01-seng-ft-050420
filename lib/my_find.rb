7a73d276544396dea085a82f5820f612866e083c
Uriah Harston <uharston@gmail.com>
2020-05-11 04:48:30 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -6,27 +6,3 @@ def my_find(collection)
     i += 1
   end
 end
-def my_find(collection)
-
-  i = 0
-  while i < collection.size
-    yield(collection[i])
-    i += 0
-  end
-end
-
-collection = (1..100).to_a
-
-my_find(collection) {|i| i % 3 == 0 and i % 5 == 0 }
-
-  i = 0  
-  while i < collection.size  
-    yield(collection[i])
-    i += 0  
-  end
-end
-
-collection = (1..100).to_a
-
-my_find(collection) {|i| i % 3 == 0 and i % 5 == 0 }
-
